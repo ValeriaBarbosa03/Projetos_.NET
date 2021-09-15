4db0c50ef9c7f87fb0922ed003139025614a89c1
ValeriaBarbosa03 <62033490+ValeriaBarbosa03@users.noreply.github.com>
2021-09-15 14:54:03 -0300

    Add files via upload

---
+++ Primeiro_Projeto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projeto_Net 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, Valéria!");
+        }
+    }
+}
